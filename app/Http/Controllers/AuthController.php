6741f365a0356517ef88924b8bb6629a7d833aa6
ThongChauPro-Coder <chauhuynhthong2022@gmail.com>
2025-05-30 23:43:33 +0700

    Authentication (Login / SignUp)

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,108 @@
+<?php
+
+namespace App\Http\Controllers;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+
+class AuthController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        return response() -> json(User::latest() -> get());
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function register(Request $request)
+    {
+        $fields = $request->validate([
+            'name' => 'required | string',
+            'email' => 'required | string | unique:users, email',
+            'password' => 'required | string'
+        ]);
+
+        $user = User::create([
+            'name' => $fields['name'],
+            'email' => $fields['email'],
+            'password' => bcrypt($fields['password']),
+        ]);
+
+        $token = $user -> createToken('webbackendapp')->plainTextToken;
+
+        $response = [
+            'user' => $user,
+            'token' => $token
+        ];
+
+        return response($response, 201);
+    }
+
+    public function login(Request $request)
+    {
+        $fields = $request->validate([
+            'email' => 'required | string | email',
+            'password' => 'required | string'
+        ]);
+
+        $user = User::where('email', $fields['email']) -> first();
+
+        if(!$user || !Hash::check($fields['password'], $user->password)){
+            return response([
+                'message' => 'Bad credentials'
+            ], 401);
+        }
+
+        $token = $user -> createToken('webbackendapp')->plainTextToken;
+
+        $response = [
+            'user' => $user,
+            'token' => $token
+        ];
+
+        return response($response, 201);
+    }
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
